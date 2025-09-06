337485a5aa0644c50f1da2df63d3e12b9e8eed2d
Ryan Greenup <ryanxgreenup@gmail.com>
2025-09-06 23:43:32 +1000

    Add Tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,43 @@
-import { mergeConfig } from 'vite';
+import { mergeConfig } from "vite";
 
-import type { StorybookConfig } from 'storybook-solidjs-vite';
+import type { StorybookConfig } from "storybook-solidjs-vite";
 
 export default <StorybookConfig>{
-    framework: 'storybook-solidjs-vite',
-    addons: [
-        '@storybook/addon-onboarding',
-        '@storybook/addon-docs',
-        '@storybook/addon-a11y',
-        '@storybook/addon-links',
-        {
-            name: '@storybook/addon-vitest',
-            options: {
-                cli: false,
-            },
-        },
-    ],
-    stories: [
-        '../stories/**/*.mdx',
-        '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    ],
-    async viteFinal(config) {
-        return mergeConfig(config, {
-            define: {
-                'process.env': {},
-            },
-        });
+  framework: "storybook-solidjs-vite",
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-links",
+    {
+      name: "@storybook/addon-vitest",
+      options: {
+        cli: false,
+      },
     },
-    docs: {
-        autodocs: true,
+  ],
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [(await import("@tailwindcss/vite")).default()],
+      define: {
+        "process.env": {},
+      },
+    });
+  },
+  docs: {
+    autodocs: true,
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      // 👇 Default prop filter, which excludes props from node_modules
+      propFilter: (prop: any) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
-    typescript: {
-        reactDocgen: 'react-docgen-typescript',
-        reactDocgenTypescriptOptions: {
-            shouldExtractLiteralValuesFromEnum: true,
-            // 👇 Default prop filter, which excludes props from node_modules
-            propFilter: (prop: any) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-        },
-    },
+  },
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from 'storybook-solidjs-vite';
+import "../src/app.css";
 
 const preview: Preview = {
     tags: ['autodocs'],
